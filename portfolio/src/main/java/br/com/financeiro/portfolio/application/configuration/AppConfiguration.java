5f4283a77fe1d9cd0bc4ff94afaf1a0d869bc00f
Felipe Abreu <felipeabreu.rj@gmail.com>
2022-08-25 15:52:04 -0300

    Adicionando a classe AppConfiguration para configurações gerais da aplicação

---
+++ portfolio/src/main/java/br/com/financeiro/portfolio/application/configuration/AppConfiguration.java
@@ -0,0 +1,33 @@
+package br.com.financeiro.portfolio.application.configuration;
+
+import java.time.Duration;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class AppConfiguration {
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+        messageSource.setBasenames(
+                "classpath:/messages/error_messages", 
+                "classpath:/messages/response_messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return messageSource;
+    }
+
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder
+                .setConnectTimeout(Duration.ofSeconds(30))
+                .setReadTimeout(Duration.ofSeconds(30))
+                .build();
+    }
+
+}
